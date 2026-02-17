311a630a281321fc5d37578006e11ac1545e83c9
Stefan Paychère <stefan@nxlabs.ch>
2026-02-17 16:02:39 +0100

    build: update version to 1.7.0 for release

--- samples/C/version.h
+++ samples/C/version.h
@@ -2,9 +2,9 @@
 #ifndef LIB_VERSION
 
 #define API_MAJOR 1
-#define API_MINOR 6
+#define API_MINOR 7
 #define API_PATCH 0
 
-#define LIB_VERSION "1.6.0"
+#define LIB_VERSION "1.7.0"
 
 #endif  // LIB_VERSION
